b25106b21530a29d25d298a5c69127914548539f
srinaththota <srinath.thota@gmail.com>
2022-01-03 19:45:30 +0530

    solved logical error

--- src/store/actions.js
+++ src/store/actions.js
@@ -49,10 +49,10 @@ export const getAllCredsAction = (data)=>{
   return async dispatch => {
     try{
             const request=new GetCredentialsRequest();
-            request.setUserid(data.userid);
-            request.setLimit(data.limit)
-            request.setAfter(data.after)
-            Client.getCredentials(request, {}, (err, response) => {
+            request.setUserid("23cb1450-4aaa-4f98-aa70-e3320d914178");
+            request.setLimit(5)
+            request.setAfter(1)
+            Client.getUsers(request, {}, (err, response) => {
             if (response == null) {
               console.log(err.message)
                 dispatch({
